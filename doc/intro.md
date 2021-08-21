d01473c4a95505341c13185b36bf457fa5b36962
Bryan Christopher Maass <bryan.maass@gmail.com>
2021-08-20 20:14:49 -0500

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to memory-whole
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/memory_whole/core_test.clj
@@ -0,0 +1,7 @@
+(ns memory-whole.core-test
+  (:require [clojure.test :refer :all]
+            [memory-whole.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
