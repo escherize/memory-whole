c9252657a7964c107ceb2d77fc6c6600a792e365
Bryan Christopher Maass <bryan.maass@gmail.com>
2021-08-29 23:38:06 -0500

    bump version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject memory-whole "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject escherize/memory-whole "0.1.0"
+  :description "Tracing to remember function args and return values"
+  :url "https://github.com/escherize/memory-whole"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
